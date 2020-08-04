031d2b9171b12147a44aead0f4ed51b1b92f4f41
ac2pic <ac2pic@gmail.com>
2020-08-04 12:56:08 -0500

    Simplified code

--- qine/mod.js
+++ qine/mod.js
@@ -1,78 +1,42 @@
-sc.CommonEvents.inject({
-	_loadCommonEvents: function() {
-		// let it do its thing
-		this.parent();
-		// going to find the indexOf no-contact
-		const socialActions = this.eventsByType["SOCIAL_ACTION"];
-		let i = 0;
-		for (i = 0; i < socialActions.length; i++) {
-			if (socialActions[i].name === "nobody-contact") {
-				break;
-			}
-		}
-		let noContactIndex = i;
-		// we have the index now find all the -contact and splice it
-		++i;
-		for (; i < socialActions.length;i++) {
-			if (socialActions[i].name.endsWith("-contact")) {
-				socialActions.splice(noContactIndex, 0, socialActions.splice(i, 1).pop());
-			}
-		}
-	}
+ig.ACTION_STEP.SET_SCREEN_ENEMY_TARGET = ig.ActionStepBase.extend({
+  target: 0,
+  _wm: new ig.Config({
+    attributes: {
+      target: {
+        _type: "Entity",
+        _info: "Entity to target",
+        _withNull: true
+      }
+    }
+  }),
+  init: function(a) {
+    this.target = a.target
+  },
+  run: function(a) {
+    var c = ig.Event.getEntity(this.target, undefined);
+    sc.combat.setScreenEnemiesTarget(c);
+  },
 });
 
-class Qine {
-  constructor() {
-	  this._init();
-  }
+sc.COMBAT_MSG_TYPE.GUARD_COUNTER = {
+  icon: "",
+  msg: "sc.gui.combat-msg.guard-counter",
+  keepPos: true,
+  duration: 1
+};
 
-  _init() {
-    this._addCombatAction();
-    this._addCombatMessage();
-    this._addPartyMember();
-  }
 
-  _addCombatAction() {
-    ig.ACTION_STEP.SET_SCREEN_ENEMY_TARGET = ig.ActionStepBase.extend({
-      target: 0,
-      _wm: new ig.Config({
-        attributes: {
-          target: {
-            _type: "Entity",
-            _info: "Entity to target",
-            _withNull: true
-          }
-        }
-      }),
-      init: function(a) {
-        this.target = a.target
-      },
-      run: function(a) {
-        var c = ig.Event.getEntity(this.target, undefined);
-        sc.combat.setScreenEnemiesTarget(c);
-      },
-    });
+ig.merge(ig.BGM_TRACK_LIST, {
+  "qine": {
+      "path": "media/bgm/muQine.ogg",
+      "loopEnd": 80.02,
+      "volume": 2
+  },
+  "unbreakable": {
+      "intro": "media/bgm/muUnbreakable-i.ogg",
+      "path": "media/bgm/muUnbreakable.ogg",
+      "introEnd": 5.537,
+      "loopEnd": 74.605,
+      "volume": 2
   }
-
-  _addCombatMessage() {
-    sc.COMBAT_MSG_TYPE.GUARD_COUNTER = {
-      icon: "",
-      msg: "sc.gui.combat-msg.guard-counter",
-      keepPos: true,
-      duration: 1
-    };
-  }
-
-  _addPartyMember() {
-    /*
-     * The idea here is to add a new entry to the list of party members,
-     * and then call the functions that reference that list to reinit the
-     * list of data. The way I've implemented this *should* allow for
-     * multiple concurrent new party member mods to exist and work correctly
-     * with each other.
-     */
-    sc.PARTY_OPTIONS.push("Qine");
-  }
-}
-
-new Qine();
+})
